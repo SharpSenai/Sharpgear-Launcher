a7848c2c8d225a2e911e0ab15b387456a5e75e24
thaue kleber de oliveira <thauekleber@gmail.com>
2024-11-21 10:06:57 -0300

    21/11

---
+++ sharpgear-ui/python/loja.py
@@ -0,0 +1,12 @@
+import customtkinter as ctk
+import sqlite3
+from biblioteca import UpperFrame
+
+
+
+class LoginFrame(ctk.CTkFrame):
+    def __init__(self,master):
+        super().__init__(master,)
+
+
+        
